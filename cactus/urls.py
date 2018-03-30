93511439881577b1c2d3de7dd16429ba26d2a4af
ThierryCFAEpure <37153802+ThierryCFAEpure@users.noreply.github.com>
2018-03-30 12:16:25 +0200

    Add files via upload

--- cactus/urls.py
+++ cactus/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from saisiecontrat.views import login,premierepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',premierepage),
+    path('login/',login),
+    path('premierepage/',premierepage),
 ]
